e87c2c6c636d3e566abe86d4985306c21903bd4c
Yamkela Venfolo <yamkela.venfolo@SAMAC137.local>
2018-02-16 12:55:48 +0200

    React Challenge progress

--- src/components/Horse/Horse.js
+++ src/components/Horse/Horse.js
@@ -4,12 +4,31 @@ import Progress from 'react-progressbar';
 
 export class Horse extends Component {
 
+constructor(props){
+    super(props);
+    this.state = {
+      interval: Math.floor(Math.random() * 500),
+      progress: 0,
+    }
+}
+
+componentDidMount = () => {
+    this.interval = setInterval(this.timer, this.state.interval);
+}
+
+timer = () => {
+    this.setState({ progress: this.state.progress + 1 });
+    console.log("anyhting");
+    (this.state.progress >= 99) ? this.setState({ progress: 100 }) : "" ;  
+}
+
+
   render(){
     return (
       < div className="RaceTrackBackground">
         <img className = "picture" id= {this.props.Id} src = {this.props.avator} alt = "avator"/> 
         <div className = "ProgressBar" >
-        <Progress className = "Progress" color = {this.props.color} completed={this.props.progress}/>  
+        <Progress className = "Progress" color = {this.props.color} completed={this.state.progress}/>  
         </div>
       </div>
     );
